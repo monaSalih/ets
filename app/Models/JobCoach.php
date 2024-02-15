617a218b51a13be478c39ec91dc4327f3954d6b9
AmroAl-Wageei <shandaqamro@gmail.com>
2024-02-15 16:46:55 +0300

    add new role  table with one to one  relation with job coach table connect user_id in role table with id in job coach table collaberate with mona

--- app/Models/JobCoach.php
+++ app/Models/JobCoach.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Role;
+
 
 class JobCoach extends Model
 {
     use HasFactory;
+
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class, 'user_id', 'id');
+    }
 }
---
+++ app/Models/Role.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\JobCoach;
+
+class Role extends Model
+{
+    use HasFactory;
+    protected $fillable = [  "id" , "user_role" ];
+
+
+    public function jobCoach()
+    {
+        return $this->hasOne(JobCoach::class, 'id', 'user_id');
+    }
+}
